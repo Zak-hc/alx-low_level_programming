e5baba2be0d0720551f1a27735748b857e7726e3
Zack-hc <zhaci001@gmail.com>
2023-03-16 11:04:17 -0700

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - Prints a message to the console
+ * Return: 0 (Success)
+ */
+int main() 
+{printf("with proper grammar, but the outcome is a piece of art,")
+retur (0); }
