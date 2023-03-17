8d2b6060fe718bbee594936da94fcac4acfac0ed
Zack-hc <zhaci001@gmail.com>
2023-03-17 06:45:49 -0700

    task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+printf("%i is negative");
+if (n == 0)
+printf("%i is zero");
+if (n > 0)
+printf("%i is positive");
+return (0);
+}
