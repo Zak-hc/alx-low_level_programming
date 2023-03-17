383cb90ae5bcb2245fdaa4a9b007b800eb88eb8a
Zack-hc <zhaci001@gmail.com>
2023-03-17 08:36:01 -0700

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{int i;
+for (i = 65; i <= 122; i++)
+{putchar(i); }
+putchar('\n');
+return (0); }
