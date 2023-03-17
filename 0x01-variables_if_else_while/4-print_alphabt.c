bc53ef8c7e98516f5de94ed3a3b8b01ee8cbcc97
Zack-hc <zhaci001@gmail.com>
2023-03-17 15:56:29 -0700

    task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{int i;
+for (i = 97; i <= 122; i++)
+{
+if (i == 101 || i == 113; i++)
+continue;
+putchar(i); }
+putchar('\n');
+return (0); }
