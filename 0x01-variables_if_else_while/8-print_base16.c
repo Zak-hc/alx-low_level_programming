0e8f908b5994dd200d7f0ee636148ef57a47a1e9
Zack-hc <zhaci001@gmail.com>
2023-03-17 16:41:19 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{int i;
+for (i = 0; i < 10; i++)
+{putchar(i + '0'); }
+for (i = 97; i <= 102; i++)
+{putchar(i); }
+putchar('\n');
+return (0); }
