6ac2f121ea22ace51b0855b582d57a5ff0f3f54d
Zack-hc <zhaci001@gmail.com>
2023-03-17 16:25:03 -0700

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Function of hhsdhj
+ * Return: 0 (Success)
+ */
+int main(void)
+{int i;
+for (i = 122; i >= 97; i--)
+putchar(i); }
+putchar('\n');
+return (0); }
