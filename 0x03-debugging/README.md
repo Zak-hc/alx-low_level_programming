6982ac5f0cbb6c8ba28cc151e9a6bbcbb498c02f
Zack-hc <zhaci001@gmail.com>
2023-03-23 09:52:43 -0700

    hi

--- 0x03-debugging/README.md
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-level 0--+++
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * positive_or_negative - check it
+ * positiveornegative - checks the sign of a given number
  *
- * @n: check number
- * Return: 0 (Success)
+ * @n: the number to be checked
+ *
+ * Return: Always 0 (Success)
  */
-void positive_or_negative(int n)
+int positive_or_negative(int n)
 {
-if (n < 0)
-printf("%d is negative\n", n);
+if (n > 0)
+printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
 else
-printf("%d is positive\n", n);
+printf("%d is negative\n", n);
+return (0);
 }
