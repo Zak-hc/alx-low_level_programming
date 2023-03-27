a827852397222d81e6ff957b9ed4239c0958672d
Zack-hc <zhaci001@gmail.com>
2023-03-27 16:22:32 -0700

    task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 /**
  * _strlen-var in 
  *
- * @s[]: is a var
+ * @s: is a var
  *
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
 int i;
-for (i = 0; s[i] != '\0'; i++);
+for (i = 0; s[i] != '\0'; i++)
+;
 return (i);
 }
