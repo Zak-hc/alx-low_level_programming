eb67f8b4d94456bb1f2b141cf72897b82a540b58
Zack-hc <zhaci001@gmail.com>
2023-03-27 16:07:43 -0700

    task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * reset_to_98- check the codefff
+ *
+ * n is a var
+ *
+ * @n: var in 98
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++);
+return(i);
+}
