b19592529cc00ce9b7be2b840873f40c7f884d1d
Zack-hc <zhaci001@gmail.com>
2023-03-28 08:25:47 -0700

    task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string-check the code
+ * @s: var in 98
+ *
+ * Return: Always 0.
+ */
+#include "main.h"
+void rev_string(char *s)
+{
+int i;
+int y;
+int j=0;
+char c;
+for (i = 0; s[i] != '\0';
+i++)
+{
+}
+for (y  = i - 1; y > j;
+y--)
+{
+c = s[j];
+s[j] = s[y];
+s[y] = c;
+j++;
+}
+}
