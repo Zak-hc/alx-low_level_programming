981dbcfe19b4f7f051d76bd66040ef2583cb89af
Zack-hc <zhaci001@gmail.com>
2023-03-28 10:28:22 -0700

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half-check the cod
+ * @str: var in 98
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int i;
+int j;
+for (i = 0; str[i] != '\0';
+i++)
+{
+}
+for (j = i / 2; j < i ;
+j++)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
