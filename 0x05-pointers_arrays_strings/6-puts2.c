a33a3b1ad31e83520afeb04e184d828b0e78ac4b
Zack-hc <zhaci001@gmail.com>
2023-03-28 08:36:46 -0700

    task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+/**
+ * _puts2-check the code
+ * @str: var in 98
+ *
+ * Return: Always 0.
+ */
+#include "main.h"
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0';
+i+=2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
