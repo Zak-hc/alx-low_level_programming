9d8aa9fb0d8befe7586df19f97010e57a493be4c
Zack-hc <zhaci001@gmail.com>
2023-03-28 17:09:27 -0700

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "main.h"
+#include "string.h"
+/**
+ * _strcpy- check the cod
+ * @dest: var in 98
+ * @src: varrrrr
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[strlen(src)] = '\0';
+return dest;
+}
