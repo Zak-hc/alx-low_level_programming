870efdb9cfa892aeb72768982d70fa8b3bbd1332
Zack-hc <zhaci001@gmail.com>
2023-03-29 15:02:43 -0700

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strcat- converts a string to an integer
+ * @dest: string to be converted
+ * @src: jfjffkkfk
+ * Return: the int converted from the string
+ */
+
+
+
+
+char *_strcat(char *dest, char *src){
+int i;
+int j;
+char *z;
+for (i = 0; str[i] < '\0'; i++)
+{
+}
+for(j = 0; dest[j] < '\0'; j++)
+}
+}
+z[]=dest[j]+str[i]
+return(z) ;
+}
