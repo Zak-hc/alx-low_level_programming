4cefcba67ba93f7db932670101a695588b55059a
Zack-hc <zhaci001@gmail.com>
2023-03-29 17:10:05 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,14 @@
  * Return: the int converted from the string
  */
 
-char *_strcat(char *dest, char *src){
+char *_strcat(char *dest, char *src)
+{
 int i;
 int j;
 for (i = 0; dest[i] != '\0'; i++)
 {
 }
-for(j = 0; src[j] != '\0'; j++)
+for (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j];
 i++;
