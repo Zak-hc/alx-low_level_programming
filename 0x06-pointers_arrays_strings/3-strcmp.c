e3ee51acf06be8c17e7b58e730ffd9851a45974f
Zack-hc <zhaci001@gmail.com>
2023-03-31 08:21:41 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * *_strcmp- compar string 
+ * *_strcmp- compar string
  * @s1: string to be converted
  * @s2: jfjffkkfk
  * Return: the int converted from the string
  */
-int _strcmp(char *s1, char *s2){
+int _strcmp(char *s1, char *s2)
+{
 int i;
 int s;
 for (i = 0; s1[i] != '\0' || s2[i] != '\0' ; i++)
