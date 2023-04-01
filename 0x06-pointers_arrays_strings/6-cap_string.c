84611ace1b5902ec5fb8eaf0d89a5bde2dd40e25
Zack-hc <zhaci001@gmail.com>
2023-03-31 17:25:27 -0700

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,39 @@
 #include "main.h"
 /**
- * *cap_string- converts a string to an integer
+ * cap_string- converts a string to an integer
  * @str: string to be convert
+ * is_separator- jjfjfj
+ * @c: char
+ * @separators: char
+ *
  * Return: the int converted from the string
  */
-char *cap_string(char *)
+int is_separator(char c, char *separators)
 {
-
-
+int j;
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (c == separators[j])
+{
+return (1);
 }
+}
+return (0);
+}
+char *cap_string(char *str)
+{
+int i, j;
+char separators[] = " \t\n,;.!?\"(){}";
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0 || is_separator(str[i - 1], separators))
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+}
+}
+return (str);
+}
+{
