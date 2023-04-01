4fb6df09b2972cdbfae88b3ef37e2ee54f6205fd
Zack-hc <zhaci001@gmail.com>
2023-03-31 18:07:04 -0700

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,44 @@
 #include "main.h"
+
+int check_seperators(char c);
+
 /**
- * cap_string- converts a string to an integer
- * @str: string to be convert
- *
- * Return: the int converted from the string
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
+ * Return: pointer to s
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (i == 0 || (str[i - 1] == ' ' || str[i - 1] == '\n'
-|| str[i - 1] == ',' ||
-str[i - 1] == ';' || str[i - 1] == '.' ||
-str[i - 1] == '!' || str[i - 1] == '?'
-|| str[i - 1] == '\"' || str[i - 1] == '('
-|| str[i - 1] == ')' || str[i - 1] == '{'
-|| str[i - 1] == '}'))
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
+	}
+
+	return (s);
 }
-}
-}
-return (str);
+
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }
