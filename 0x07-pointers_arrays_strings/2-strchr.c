18b66795224a05d026a4fbb6f712db61519e7a1c
Zack-hc <zhaci001@gmail.com>
2023-04-03 16:58:29 -0700

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,12 +3,12 @@
  * _strchr- converts a string to an integer
  * @s: string to be converted
  * @c: jfjffkkfk
- * Return: the int converted from the string
+ * Return:  onverted 0 from the string
  */
 char *_strchr(char *s, char c)
 {
 int v;
-for (v=0; s[v] >= '\0'; v++)
+for (v = 0; s[v] >= '\0'; v++)
 {
 if (s[v] == c)
 return (&s[v]);
