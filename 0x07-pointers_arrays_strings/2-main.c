8d7814553455761b8f407b6960fa0a841aa38049
Zack-hc <zhaci001@gmail.com>
2023-04-03 15:06:31 -0700

    task

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -6,13 +6,24 @@
  *
  * Return: Always 0.
  */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-char *s = "hello, world";
-char *f = "oleh";
-unsigned int n;
+    char *s = "hello";
+    char *f;
 
-n = _strspn(s, f);
-printf("%u\n", n);
-return (0);
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
 }
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strchr- converts a string to an integer
+ * @s: string to be converted
+ * @c: jfjffkkfk
+ * Return: the int converted from the string
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+int v;
+int f=0;
+static char y[100];
+for (v = 0; s[v] != '\0'; v++)
+{
+if (s[v] == c)
+break;
+}
+for (i = v; s[i] != '\0'; i++)
+{
+y[f]=s[i];
+f++;
+}
+y[f] = '\0';
+return (y);
+}
