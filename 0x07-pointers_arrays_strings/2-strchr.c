b75c9a73d16776a2b393898a1d0fe7f9b56413d4
Zack-hc <zhaci001@gmail.com>
2023-04-03 16:37:32 -0700

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,26 +7,11 @@
  */
 char *_strchr(char *s, char c)
 {
-char y[100];
-int i;
 int v;
-int f = 0;
-char *p = y;
-int g;
-for (g = 0;g < 90; g++)
-{
- y[g]='\0';
-}
 for (v = 0; s[v] != '\0'; v++)
 {
 if (s[v] == c)
 break;
 }
-for (i = v; s[i] != '\0'; i++)
-{
-y[f] = s[i];
-f++;
-}
-y[f] = '\0';
-return (p);
+return (&s[v]);
 }
