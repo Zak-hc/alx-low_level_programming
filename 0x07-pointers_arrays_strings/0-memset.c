69be48b36c15a2497964ffc0cf8495c23f838d95
Zack-hc <zhaci001@gmail.com>
2023-04-03 09:40:30 -0700

    task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * *_memset- converts a string to an integer
- * @s: string to be converted
- * @b: jfjffkkfk
+ * @v: string to be converted
+ * @j: jfjffkkfk
  * @n: intg
  * Return: the int converted from the string
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *v, char j, unsigned int n)
 {
 unsigned int i;
-for(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
-s[i] = b;
+v[i] = j;
 }
-return (s);
+return (v);
 }
