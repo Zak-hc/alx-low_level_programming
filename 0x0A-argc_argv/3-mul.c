8179f1f95508c6348b7498205af0bb04107701a4
Zack-hc <zhaci001@gmail.com>
2023-04-08 11:47:08 -0700

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (int argc, char* argv[])
+int main(int argc, char *argv)
 {
 int y = 0;
 int i;
 for (i = 0; i < argc; i++)
-if (i > 1)
+if (argc > 1)
 {
 y = y * atoi(argv[i]);
 }
