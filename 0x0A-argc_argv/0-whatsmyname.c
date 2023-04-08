7a77bfd7ec4456d26dd31b8bf03c674e47134c67
Zack-hc <zhaci001@gmail.com>
2023-04-08 08:39:42 -0700

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-int main (int argc , char* argv[])
+/**
+ * main- nfnjnjdnjwnj
+ * @argc: jnjenjnejn
+ * @argv: jkdnkmsknksns
+ * Return: jjnfjnjnfj 0 hfhfhf
+ */
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
 {
-printf ("%s", argv[i]);
+printf("%s", argv[i]);
 }
-printf ("\n");
+printf("\n");
 return (0);
 }
