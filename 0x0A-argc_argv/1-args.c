c6798472ddd6d82ba079341cfcb9ac31c45ebb58
Zack-hc <zhaci001@gmail.com>
2023-04-08 11:27:21 -0700

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - print the number 
+ * @argc: An argcounter
+ * @argv: An argvalues
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 if (argv[0])
-printf("%d\n", argc - 1);
+printf("%i\n", argc - 1);
 return (0);
 }
