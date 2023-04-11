ea32041d6c6d2e4dfa16ece564d08da894e08a44
Zack-hc <zhaci001@gmail.com>
2023-04-11 15:05:43 -0700

    by me

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * *_strdup- converts ato ager
+ * @str: sng to rted
+ * Return: the int crted om e ring
+ */
+char *_strdup(char *str)
+{
+int i;
+char *z;
+int j;
+for (j = 0; str[j] != '\0'; j++)
+{
+}
+z = malloc(sizeof(char) * j);
+if (z == NULL)
+{
+return (0);
+}
+for (i = 0; i < j; i++)
+{
+z[i] = str[i];
+}
+return (z);
+}
