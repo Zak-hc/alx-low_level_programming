d1e30be6739405b733ad6cb12b91b633bb7a54a4
Zack-hc <zhaci001@gmail.com>
2023-04-11 14:36:13 -0700

    by me

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * *create_array- converts a string to an integer
+ * @size: string to be converted
+ * @c: jfjffkkfk
+ * Return: the int converted from the string
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *z;
+
+	z = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+	z[i] = c;
+	}
+	if (size == 0)
+	{
+	return (0);
+	}
+	else
+	return (z);
+}
