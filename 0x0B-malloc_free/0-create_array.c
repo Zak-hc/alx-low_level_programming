ad1dc438e72e3d26fe728a4b206bec09bc479251
Zack-hc <zhaci001@gmail.com>
2023-04-11 14:45:55 -0700

    by me

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,14 @@ char *create_array(unsigned int size, char c)
 	char *z;
 
 	z = malloc(size * sizeof(char));
+	if (z == NULL)
+	{
+	return (0);
+	}
 	for (i = 0; i < size; i++)
 	{
 	z[i] = c;
 	}
-	if (z == NULL)
-	return (0);
 	if (size == 0)
 	{
 	return (0);
