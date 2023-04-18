a12cf87ae68128267ecd3469e1aa2a5872480e01
Zack-hc <zhaci001@gmail.com>
2023-04-18 16:00:35 -0700

    by zack

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
-int main (void)
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: 0 on success
+ */
+int main(void)
 {
 const char *s = __FILE__;
 int i;
