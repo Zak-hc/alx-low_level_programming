1f889629cefe842749aeedb6a2f15f6cfb89a2cd
Zack-hc <zhaci001@gmail.com>
2023-04-18 19:03:10 -0700

    by zak

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ if (d == NULL)
 {
 return;
 }
-printf("Name: %s\n", d->name = NULL ? "(nil)" : d->name);
+printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
 printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner = NULL ? "(nil)" : d->owner);
+printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
