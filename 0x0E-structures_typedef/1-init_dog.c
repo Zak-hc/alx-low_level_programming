8c27df7f21d4daf2e840d7a48784572494b3ec79
Zack-hc <zhaci001@gmail.com>
2023-04-18 17:52:21 -0700

    by zak

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,15 @@
 #ifndef "_1_INIT_DOG_C_"
 #define "_1_INIT_DOG_C_"
 #include "dog.h"
+/**
+ * init_dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * @d: hjdjj
+ *
+ * Description: Longer description
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 d->name = name;
