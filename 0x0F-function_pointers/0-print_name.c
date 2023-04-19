3a3b89c153a5b4f305ea84590430ba223ed593f5
Zack-hc <zhaci001@gmail.com>
2023-04-19 11:23:26 -0700

    by zak

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include "stddef.h"
+/**
+ * print_name- rjjnfnf
+ * @name: hndjejk
+ * @f: jjjjd
+ * return: fjjf 0 jj
+*/
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
