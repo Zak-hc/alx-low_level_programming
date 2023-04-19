13065a60af9d1b28ec556e8ced06588ae71d820d
Zack-hc <zhaci001@gmail.com>
2023-04-19 11:46:51 -0700

    by zak

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include "stdio.h"
+/**
+ *
+ *
+ *
+ *
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+for (size_t i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
