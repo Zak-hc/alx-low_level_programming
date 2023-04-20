2a0da563df3e1554d67a080044794a24cd2927ea
Zack-hc <zhaci001@gmail.com>
2023-04-19 19:55:20 -0700

    by zak

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,6 +9,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	/* struct opts of struct op_t */
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -19,12 +20,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 10)
+	while (i < 5)
 	{
-		if (s[0] == ops->op[i])
-			break;
+		if (*s == *ops[i].op)
+			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i / 2].f);
+	return (NULL);
 }
