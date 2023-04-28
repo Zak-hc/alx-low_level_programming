38cde5e67001dde479b90705ccab7db8f8b07e32
Zack-hc <zhaci001@gmail.com>
2023-04-27 19:33:07 -0700

    printing by ZAK HC

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_before_main - Function to print message before main is executed
+ */
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
