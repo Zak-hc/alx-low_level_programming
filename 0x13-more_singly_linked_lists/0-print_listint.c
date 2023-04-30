93f82fa7eb6891d548126c2a085c43c1af847136
Zack-hc <zhaci001@gmail.com>
2023-04-30 06:23:55 -0700

    zakn

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_listint - Print_t list.
+ * @h: A poit_t list
+ * Return: The numnodes in
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d", n);
+count++
+}
+return (count);
+}
