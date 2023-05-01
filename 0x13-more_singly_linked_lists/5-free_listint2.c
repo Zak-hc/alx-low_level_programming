5ecab7ae0db46e80b969fddc1e0becb4deefbecc
Zack-hc <zhaci001@gmail.com>
2023-05-01 08:15:13 -0700

    zakn

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a ltint_t list
+ * @head: pointer to  of the list
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *ti, *to;
+ti = *head;
+while (ti != NULL)
+{
+to = ti->next;
+free(ti);
+ti = to;
+}
+*head = NULL;
+}
