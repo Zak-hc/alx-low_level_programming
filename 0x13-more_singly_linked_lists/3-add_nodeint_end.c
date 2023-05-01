e47731d10ed8560e8ee7223359058e19e5bb22e4
Zack-hc <zhaci001@gmail.com>
2023-05-01 07:11:28 -0700

    zakn

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *fr;
 listint_t *t = *head;
 fr = malloc(sizeof(listint_t));
-if (fr == NULL || head == NULL)
+if (fr == NULL)
 {
 return (NULL);
 }
