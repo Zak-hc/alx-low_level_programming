dbe5b9f3ec9da372e71a3578346327348fa0baa5
Zack-hc <zhaci001@gmail.com>
2023-06-15 10:09:05 -0700

    printing by us

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stddef.h>
 /**
- * print_dlistint - Prints all the elements of a doubly linked list.
+ * dlistint_len - Prints all the elements of a doubly linked list.
  * @h: Pointer to the head of the linked list.
  *
  * Return: The number of nodes in the linked list.
