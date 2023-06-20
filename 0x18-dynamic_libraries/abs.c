e2cf4078b45164dd4837034ca21e6f39f5da47de
Zack-hc <zhaci001@gmail.com>
2023-06-19 17:59:04 -0700

    printing by us

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute valu dhdhdfy dhud
+ * @n: the integer to compute ghgsgsyt yyys
+ * Return: the absolute value of hshsyg ggdgd
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
---
+++ 0x18-dynamic_libraries/isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - checks if a digijhhhh
+ * @c: the character hnh
+ * Return: 1 if c is a digit
+ *
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+
+{
+	return (1);
+}
+
+else
+
+{
+	return (0);
+}
+}
